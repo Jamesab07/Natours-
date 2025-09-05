70ed71e8a65a1655e531c7c8bec5949b974e139b
James <jamesabah2003@gmail.com>
2025-09-05 07:42:39 +0100

    Update after pushing Natours

--- controllers/handlerFactory.js
+++ controllers/handlerFactory.js
@@ -1,15 +1,15 @@
-const catchAsync = require('../utils/catchAsync');
-const AppError = require('../utils/appError');
-const APIfeatures = require('../utils/apiFeatures');
+const catchAsync = require("../utils/catchAsync");
+const AppError = require("../utils/appError");
+const APIfeatures = require("../utils/apiFeatures");
 
 exports.deleteOne = (Model) =>
   catchAsync(async (req, res, next) => {
     const doc = await Model.findByIdAndDelete(req.params.id, req.body);
     if (!doc) {
-      return next(new AppError('No Document Found With This ID', 404));
+      return next(new AppError("No Document Found With This ID", 404));
     }
     res.status(204).json({
-      status: 'success',
+      status: "success",
       data: null,
     });
   });
@@ -21,10 +21,10 @@ exports.updateOne = (Model) =>
       runValidators: true,
     });
     if (!doc) {
-      return next(new AppError('No document Found With This ID', 404));
+      return next(new AppError("No document Found With This ID", 404));
     }
     res.status(200).json({
-      status: 'success',
+      status: "success",
       data: {
         data: doc,
       },
@@ -38,7 +38,7 @@ exports.createOne = (Model) =>
     const doc = await Model.create(req.body);
 
     res.status(201).json({
-      status: 'success',
+      status: "success",
       data: {
         data: doc,
       },
@@ -53,10 +53,10 @@ exports.getOne = (Model, popOption) =>
     const doc = await query;
 
     if (!doc) {
-      return next(new AppError('No Tour Found With This ID', 404));
+      return next(new AppError("No Tour Found With This ID", 404));
     }
     res.status(200).json({
-      status: 'success',
+      status: "success",
       data: {
         data: doc,
       },
@@ -75,9 +75,16 @@ exports.getAll = (Model) =>
       .paginate();
     const doc = await features.query;
 
+    //   exports.createFilterObj = (req, res, next) => {
+    //     let filterObject = {};
+    //     if (req.params.productId) filterObject = {product: req.params.productId};
+    //     req.filterObj = filterObject;
+    //     next();
+    // };
+
     //SEND RESPONSE
     res.status(200).json({
-      status: 'success',
+      status: "success",
       result: doc.length,
       data: {
         data: doc,
